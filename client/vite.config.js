ec297a814074658b21aa2a46076e9a93b87202dc
Aditya Nitin Burse (Jain) <60445026+imaditya-jain@users.noreply.github.com>
2025-01-09 12:12:26 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:5000",
+        target: "https://classyshop-1jkb.vercel.app/",
         changeOrigin: true,
         secure: false
       }
